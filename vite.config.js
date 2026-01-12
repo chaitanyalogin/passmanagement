89a8808f241372d0435cba46e96872fb97128c18
chayy <chaitanyarocks.kulkarni@gmail.com>
2026-01-12 14:56:15 +0530

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/passmanagement/',   // 👈 MUST match repo name
   plugins: [
+    react(),
     tailwindcss(),
   ],
 })
